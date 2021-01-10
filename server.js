3c4d953a094028b988c8fb33492039516c67c11f
chaitanya1999 <cnjv1999@gmail.com>
2021-01-10 14:15:05 +0530

    gradient

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 80
 app.get('/', (req, res) => {
   res.sendFile('/rubecube.html' , { root : __dirname});
 })
+app.get('/gradient',(req,res)=>{
+  res.sendFile('/gradient.png' , { root : __dirname});
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
