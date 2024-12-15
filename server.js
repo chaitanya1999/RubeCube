9e3a5cfa7edd4408134cc615d993abcbd6afbac7
Chaitanya V <cnjv1999@gmail.com>
2024-12-16 00:40:47 +0530

    github pages

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const port = process.env.PORT || 80
 app.get('/', (req, res) => {
   res.sendFile('/index.html' , { root : __dirname});
 })
-app.get('/res/gradient.png',(req,res)=>{
-  res.sendFile('/res/gradient.png' , { root : __dirname});
+app.get('/RubeCube/gradient.png',(req,res)=>{
+  res.sendFile('/RubeCube/gradient.png' , { root : __dirname});
 })
 app.get('/sound.mp3',(req,res)=>{
-  res.sendFile('/sound.mp3' , { root : __dirname});
+  res.sendFile('/RubeCube/sound.mp3' , { root : __dirname});
 })
-app.get('/res/:file',(req,res)=>{
-  res.sendFile('/res/'+req.params.file , { root : __dirname});
+app.get('/RubeCube/:file',(req,res)=>{
+  res.sendFile('/RubeCube/'+req.params.file , { root : __dirname});
 })
 
 app.listen(port, () => {
